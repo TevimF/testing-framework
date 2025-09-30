c467aba3c00331789d11b6ac8dcbee663ba5d50e
Estêvão Fonseca <115885688+TevimF@users.noreply.github.com>
2025-09-30 14:11:58 -0300

    Adição da classe de resultados dos testes

--- testing_framework/test_case.py
+++ testing_framework/test_case.py
@@ -1,3 +1,6 @@
+from .test_result import TestResult
+
+
 class TestCase:
     """
     Cada método de teste em uma subclasse de TestCase é executado como um teste individual.
@@ -9,19 +12,32 @@ class TestCase:
         """
         self.test_method_name = test_method_name
 
-    def run(self):
+    def run(self, result: TestResult):
         """
         Ciclo de vida completo de um teste: a configuração, o método de teste e a limpeza.
         """
         # 1. Prepara o ambiente de teste
+        # 1.1 teste iniciado
+        result.test_started()
+        # 1.2 configuração do ambiente
         self.set_up()
 
         # 2. Obtém o método de teste real a partir do seu nome e o executa
-        test_method = getattr(self, self.test_method_name)
-        test_method()
+        try:
+            test_method = getattr(self, self.test_method_name)
+            test_method()
+        except AssertionError as e:
+            # falha na asserção (resultado esperado diferente do resultado atual)
+            result.add_failure(self.test_method_name)
+        except Exception as e:
+            # erro inesperado (exceção não tratada)
+            result.add_error(self.test_method_name)
 
         # 3. Limpa o ambiente de teste após a execução
+        # tear down
         self.tear_down()
+
+
 
     def set_up(self):
         """
--- tests/example_my_test.py
+++ tests/example_my_test.py
@@ -1,4 +1,5 @@
 from testing_framework.test_case import TestCase
+from testing_framework.test_result import TestResult
 
 class MyTest(TestCase):
     """
@@ -23,15 +24,21 @@ class MyTest(TestCase):
 
 
 if __name__ == '__main__':
+    test_result = TestResult()
+
     print("--- test_a ---")
     test_a = MyTest("test_a")
-    test_a.run()
+    test_a.run(test_result)
 
     print("--- test_b ---")
     test_b = MyTest("test_b")
-    test_b.run()
+    test_b.run(test_result)
 
     print("--- test_c ---")
     test_c = MyTest("test_c")
-    test_c.run()
+    test_c.run(test_result)
+
+    print(test_result.summary())
+
+
 
