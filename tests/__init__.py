0426d6ed0f99ac499eb676bf4fb9fc73aea75dd7
Estêvão Fonseca <115885688+TevimF@users.noreply.github.com>
2025-09-30 16:43:35 -0300

    Adição da função para execução de todos os testes

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,24 @@
-# Pacote de testes do framework
+from testing_framework.test_loader import TestLoader
+from testing_framework.test_suite import TestSuite
+from testing_framework.test_runner import TestRunner
+from tests.test_case_test import TestCaseTest
+from tests.test_suite_test import TestSuiteTest
+from tests.test_loader_test import TestLoaderTest
 
+
+def run_all_tests():
+    loader = TestLoader()
+    test_case_suite = loader.make_suite(TestCaseTest)
+    test_suite_suite = loader.make_suite(TestSuiteTest)
+    test_load_suite = loader.make_suite(TestLoaderTest)
+
+    suite = TestSuite()
+    suite.add_test(test_case_suite)
+    suite.add_test(test_suite_suite)
+    suite.add_test(test_load_suite)
+
+    runner = TestRunner()
+    runner.run(suite)
+
+if __name__ == '__main__':
+    run_all_tests()
